fdfb42817739dd9fdfa96f0a76bee18869bc6989
liuduo <272647602@qq.com>
2022-10-24 13:12:10 +0800

    add project files

---
+++ WeiboTop/WeiboTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiboTopApp.swift
+//  WeiboTop
+//
+//  Created by liuduo on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeiboTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
