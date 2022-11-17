c12a29141a6d38324dee0e281ab11882009e9daa
liuduo <272647602@qq.com>
2022-11-17 12:32:50 +0800

    Add dependency

--- Business/Package.swift
+++ Business/Package.swift
@@ -15,7 +15,8 @@ let package = Package(
             targets: ["Business"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0"))
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
+        .package(path: "../Infrastructure")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +24,8 @@ let package = Package(
         .target(
             name: "Business",
             dependencies: [
-                .product(name: "CombineMoya", package: "Moya")
+                .product(name: "CombineMoya", package: "Moya"),
+                "Infrastructure"
             ]),
         .testTarget(
             name: "BusinessTests",
